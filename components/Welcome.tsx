d24f9f3c2a3aad025008bcd3c80d474294ac1e18
Than <tan95685@gmail.com>
2023-12-24 20:08:53 +0700

    first commit

---
+++ components/Welcome.tsx
@@ -0,0 +1,154 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import { ethers } from "ethers";
+import { contractABI, contractAddress } from "./util/constants";
+
+function Welcome() {
+  const [address, setaddress] = useState("");
+  const [amount, setamount] = useState(0);
+
+  const [wallet, setWallet] = useState("");
+  const [signer, setSigner] = useState(undefined);
+  async function execute() {
+    if (typeof window.ethereum !== "undefined") {
+      const ContractAddress = contractAddress;
+      const abi = contractABI;
+      const contract = new ethers.Contract(ContractAddress, abi, signer);
+      try {
+        await contract.transfer(address, amount);
+        // await contract.transfer({
+        //   recipient: address,
+        //   amount: amount,
+        // });
+      } catch (error) {
+        console.log(error);
+      }
+    } else {
+      console.log("Please install MetaMask");
+    }
+  }
+  useEffect(() => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    setSigner(provider.getSigner());
+    getCurrentWalletConnected();
+    addWalletListener();
+  }, [wallet]);
+
+  const handleSubmit = (e: any) => {
+    execute();
+    e.preventDefault();
+  };
+
+  const getCurrentWalletConnected = async () => {
+    if (typeof window != "undefined" && typeof window.ethereum != "undefined") {
+      try {
+        const accounts = await window.ethereum.request({
+          method: "eth_accounts",
+        });
+        if (accounts.length > 0) {
+          setWallet(accounts[0]);
+          console.log(accounts[0]);
+        } else {
+          console.log("Connect to MetaMask using the Connect button");
+        }
+      } catch (err) {
+        console.error(err.message);
+      }
+    } else {
+      /* MetaMask is not installed */
+      console.log("Please install MetaMask");
+    }
+  };
+
+  const addWalletListener = async () => {
+    if (typeof window != "undefined" && typeof window.ethereum != "undefined") {
+      window.ethereum.on("accountsChanged", (accounts) => {
+        setWallet(accounts[0]);
+        console.log(accounts[0]);
+      });
+    } else {
+      /* MetaMask is not installed */
+      setWallet("");
+      console.log("Please install MetaMask");
+    }
+  };
+
+  const connectWallet = async () => {
+    if (typeof window != "undefined" && typeof window.ethereum != "undefined") {
+      try {
+        /* MetaMask is installed */
+        const accounts = await window.ethereum.request({
+          method: "eth_requestAccounts",
+        });
+        setWallet(accounts[0]);
+        console.log(accounts[0]);
+      } catch (err) {
+        console.error(err.message);
+      }
+    } else {
+      /* MetaMask is not installed */
+      console.log("Please install MetaMask");
+    }
+  };
+  return (
+    <div className=" w-screen h-100 flex flex-col justify-center items-center">
+      <span>Side Project </span>
+      <span>Conect MetaMask & tranfer PSUC</span>
+      <button
+        className=" w-[50%] h-[50%] border bg-gray-300 "
+        onClick={connectWallet}
+      >
+        connect wallet
+      </button>
+      <div className=" w-[50%] h-[20%]">
+        <div className=" flex justify-center ">
+          <div className=" px-4">Your address :</div>
+          <span className="is-link has-text-weight-bold">
+            {wallet && wallet.length > 0
+              ? `Connected: ${wallet.substring(0, 6)}...${wallet.substring(38)}`
+              : "Please connect the wallet"}
+          </span>
+        </div>
+        <form
+          onSubmit={handleSubmit}
+          className=" flex flex-col  justify-center items-center gap-2
+        "
+        >
+          <label>
+            address :
+            <input
+              type="text"
+              className=" border border-3 border-black"
+              value={address}
+              onChange={(e) => setaddress(e.target.value)}
+            />
+          </label>
+
+          <label>
+            amount :
+            <input
+              type="number"
+              className=" border border-3 border-black"
+              value={amount}
+              onChange={(e) => setamount(e.target.value)}
+            />
+          </label>
+
+          {false ? (
+            <></>
+          ) : (
+            <button
+              type="submit"
+              className=" w-20 border border-3 border-red-600"
+            >
+              Submit
+            </button>
+          )}
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Welcome;
---
+++ components/util/constants.tsx
@@ -0,0 +1,4 @@
+import abi from './Transaction.json';
+
+export const contractABI = abi.abi;
+export const contractAddress = "0xA198C12b57081f04d7AaaAbE2380Fe0316429a10";
